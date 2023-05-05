7410de2eeed447545e2713e661803f2560427e9f
newser101 <nepumuk101@gmail.com>
2023-05-05 21:45:59 +0200

    edit todocomments.lua

--- lua/plugins/todocomments.lua
+++ lua/plugins/todocomments.lua
@@ -1,21 +1,15 @@
 return {
-"folke/todo-comments.nvim",
-version ="*",
-
-config = function()
-  require("todo-comments").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      -- TODO: 
-      vim.keymap.set("n", "]t", function()
-  require("todo-comments").jump_next()
-end, { desc = "Next todo comment" }),
-
-  vim.keymap.set("n", "[t", function()
-      require("todo-comments").jump_prev()
-    end, { desc = "Previous todo comment" }),
-    }
-
-end
+  "folke/todo-comments.nvim",
+  cmd = { "TodoTrouble", "TodoTelescope" },
+  event = { "BufReadPost", "BufNewFile" },
+  config = true,
+  -- stylua: ignore
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+    { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+    { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+    { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+  },
 }
