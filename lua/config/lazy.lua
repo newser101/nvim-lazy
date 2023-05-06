073e473a83c4f3dbd84cfc80b618fd48b0128c1d
newser101 <nepumuk101@gmail.com>
2023-05-06 22:22:15 +0200

    comit

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -4,35 +4,35 @@ if not vim.loop.fs_stat(lazypath) then
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
---vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+--vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup("plugins",{
-defaults = {
-		lazy = true,
-		version = "*", -- try installing the latest stable version for plugins that support semver
-	},
+--defaults = {
+--		lazy = true,
+--		version = "*", -- try installing the latest stable version for plugins that support semver
+--	},
 	--  install = { colorscheme = { "tokyonight", "habamax" } },
 	checker = { enabled = true }, -- automatically check for plugin updates
-  -- debug = false,
+  debug = false,
   change_detection={
-  --notify=false,
+  notify=false,
   enable=true,
   },
 	performance = {
 		rtp = {
 			-- disable some rtp plugins
 			disabled_plugins = {
-			--	"gzip",
-			--	"matchit",
-			--	"matchparen",
-			--	"netrwPlugin",
-			--	"tarPlugin",
-			--	"tohtml",
-			--	"tutor",
-			--	"zipPlugin",
+			"gzip",
+			"matchit",
+			"matchparen",
+			"netrwPlugin",
+			"tarPlugin",
+			"tohtml",
+			"tutor",
+			"zipPlugin",
 			},
 		},
 	},
