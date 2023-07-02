2d4dd16b4bf26495905cb4b24cee031943c3529f
newser101 <nepumuk101@gmail.com>
2023-07-02 12:32:57 +0200

    clean illuminate

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,53 +1,46 @@
-
 return {
   "RRethy/vim-illuminate",
   event = "VeryLazy",
+  enable = true,
 
   ------------ config ----------------
-  config=function ()
-   vim.g.Illuminate_ftblacklist = { "alpha", "NvimTree" }
-   vim.api.nvim_set_keymap(
-     "n",
-     "<a-n>",
-     '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>',
-     { noremap = true }
-   )
-  vim.api.nvim_set_keymap(
-    "n",
-    "<a-p>",
-    '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>',
-    { noremap = true }
-  )
-  ------------- setup -------------
-  require('illuminate').configure({
-providers = {
-       "lsp",
-       "treesitter",
-       "regex",
-     },
-     delay = 200,
-     filetypes_denylist = {
-       "dirvish",
-       "fugitive",
-       "alpha",
-       "NvimTree",
-       "packer",
-       "neogitstatus",
-       "Trouble",
-       "lir",
-       "Outline",
-       "spectre_panel",
-       "toggleterm",
-       "DressingSelect",
-       "TelescopePrompt",
-     },
-     filetypes_allowlist = {},
-     modes_denylist = {},
-     modes_allowlist = {},
-     providers_regex_syntax_denylist = {},
-     providers_regex_syntax_allowlist = {},
-     under_cursor = true,
+  config = function()
+    vim.g.Illuminate_ftblacklist = { "alpha", "NvimTree" }
+    ---- set keymaps ----
+    local keymap=vim.keymap.set
+    local opts={noremap=true}
+    keymap("n","<a-n>",'<cmd>lua require"illuminate".next_reference{wrap=true}<cr>',opts)
+    keymap("n","<a-p>",'<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>',opts)
+    ------------- setup -------------
+    require('illuminate').configure({
+      providers = {
+        "lsp",
+        "treesitter",
+        "regex",
+      },
+      delay = 100,
+      filetypes_denylist = {
+        "dirvish",
+        "fugitive",
+        "alpha",
+        "NvimTree",
+        "packer",
+        "neogitstatus",
+        "Trouble",
+        "lir",
+        "Outline",
+        "spectre_panel",
+        "toggleterm",
+        "DressingSelect",
+        "TelescopePrompt",
+      },
+      filetypes_allowlist = {},
+      modes_denylist = {},
+      modes_allowlist = {},
+      providers_regex_syntax_denylist = {},
+      providers_regex_syntax_allowlist = {},
+      under_cursor = true,
 
-  })
+    })
   end
 }
