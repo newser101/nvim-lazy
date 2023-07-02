1084b2aec153f6d89cf9bc03d77f074f742c8e7e
newser101 <nepumuk101@gmail.com>
2023-07-02 22:27:47 +0200

    create null-ls

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,28 @@
+return {
+	"jose-elias-alvarez/null-ls.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	version = "*",
+	enabled = true,
+
+	config = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting
+		--		local diagnostics = null_ls.builtins.diagnostics
+		--		local code_action = null_ls.builtins.code_action
+		--		local completion = null_ls.builtins.completion
+		--		local hover = null_ls.builtins.hover
+
+		null_ls.setup({
+			-- NOTE: check if needed (maybe diagnostics)
+			debug = false,
+			--      log_level = "off",  -- if off :NULLINFO didnt work
+			sources = {
+				formatting.stylua, -- lua
+				formatting.black, -- python
+				formatting.prettier, -- Angular, CSS, Flow, GraphQL, HTML, JSON, JSX, JavaScript, LESS, Markdown, SCSS, TypeScript, Vue, YAML
+			},
+		})
+	end,
+}
