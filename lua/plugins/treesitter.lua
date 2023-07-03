867dfae6e04c2c66cbc9fa4c0064edac4fed288c
newser101 <nepumuk101@gmail.com>
2023-07-03 17:02:20 +0200

    format

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,18 @@
-return{
-"nvim-treesitter/nvim-treesitter",
-build=function ()
-  require("nvim-treesitter.install").update({with_sync=true})
-end,
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = function()
+    require("nvim-treesitter.install").update({ with_sync = true })
+  end,
 
-
------- minimum setup BEGINN -------
-config =function ()
-  require("nvim-treesitter.configs").setup{
-    ensure_installed="lua",
-    sync_installed=false,
-    highlight={
-      enable=false,
-    }
-  }
-end
------- minimum setup END -------
-
+  ------ minimum setup BEGINN -------
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = "lua",
+      sync_installed = false,
+      highlight = {
+        enable = false,
+      },
+    })
+  end,
+  ------ minimum setup END -------
 }
