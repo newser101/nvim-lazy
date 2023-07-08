6b6a8396b6d8638b12f922319c7f24018d5f4e92
newser101 <nepumuk101@gmail.com>
2023-07-08 22:23:26 +0200

    add file

---
+++ lua/plugins/comments.lua
@@ -0,0 +1,10 @@
+return {
+	"numToStr/Comment.nvim",
+	--event = {"BufRead", "BufNewFile"},
+	event = "VeryLazy",
+
+	config = function()
+		require("Comment").setup()
+    -- TODO:  add Treesitter https://github.com/numToStr/Comment.nvim
+	end,
+}
