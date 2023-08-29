a967870a2dee0498acab6890b4576b5e3fc24c69
newser101 <nepumuk101@gmail.com>
2023-08-29 20:51:42 +0200

    clean autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,21 +1,19 @@
+-- https://github.com/windwp/nvim-autopairs
 return {
   "windwp/nvim-autopairs",
+  event = "InsertEnter",
   dependencies = {
     "hrsh7th/nvim-cmp",
   },
 
   config = function()
     require("nvim-autopairs").setup({
-      disable_filetype = { "TelescopePrompt", "spectre_panel" }, -- "vim" , "
-      enable_check_bracket_line = false,
-      ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+      enable_check_bracket_line = false, -- check bracket in same line
+      --
       -- ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=], -- orig
-      enable_afterquote = true,    -- add bracket pairs after quote
-      enable_moveright = true,
-      disable_in_macro = false,    --- disable when recording or executing a macro
-      map_bs = true,               -- map the <BS> key
-      map_c_w = false,             --- map <c-w> to delete a pair if possible
-      disable_in_visualblock = false, -- disable when insert after visual block mode
+      -- ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""), -- next
+      ignored_next_char = "[%w%.]", -- ignore alphanumeric and `.`
+      --
 
       ---- use Treesitter to check for a pair ----
       check_ts = true, -- treesitter integration
@@ -26,18 +24,25 @@ return {
       },
 
       --- fast_wrap
-      fastwrap = {
-        map = "<M-e>",
-        chars = { "{", "[", "(", '"', "'" },
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        offset = 0, -- Offset from pattern match
-        end_key = "$",
-        --end_key = "-",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = "Search",
-        highlight_grey = "Comment",
-      },
+      --
+      --Before        Input                    After
+      --------------------------------------------------
+      -- (|foobar      <M-e> then press $        (|foobar)
+      -- (|)(foobar)   <M-e> then press q       (|(foobar))
+      --
+      fast_wrap = {},
+      -- fastwrap = {
+      --   map = "<M-e>",
+      --   chars = { "{", "[", "(", '"', "'" },
+      --   pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+      --   offset = 0, -- Offset from pattern match
+      --   end_key = "$",
+      --   --end_key = "-",
+      --   keys = "qwertyuiopzxcvbnmasdfghjkl",
+      --   check_comma = true,
+      --   highlight = "Search",
+      --   highlight_grey = "Comment",
+      -- },
     })
     -------You need to add mapping `CR` on nvim-cmp setup-------
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
