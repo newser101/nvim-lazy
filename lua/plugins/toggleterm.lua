e4e846f62ac34454425b17c2b38e5e0d3e1d489f
newser101 <nepumuk101@gmail.com>
2023-10-15 18:20:52 +0200

    edit

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -21,45 +21,12 @@ return {
 
     local Terminal = require("toggleterm.terminal").Terminal
     local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-    local pythonrunfile = Terminal:new({ cmd = "python %", hidden = true })
-    local python = Terminal:new({ cmd = "python3 %", noremap = true, hidden = true })
-
-    -- local run = Terminal:new({
-    --   cmd = "python3.9 " .. name,
-    --   hidden = true,
-    --   direction = float,
-    --   close_on_exit = false,
-    -- })
 
     function Lazygit_toggle()
       lazygit:toggle()
     end
 
-    function Runpythonfile_toggle()
-      run:toggle()
-      -- pythonrunfile:toggle()
-    end
-
-    function _PYTHON_TOGGLE()
-      python:toggle()
-    end
-
-    function Test_toggle()
-      local name = vim.api.nvim_buf_get_name(0)
-      print("python3 " .. name)
-    end
-
-    function Test_toggle2()
-      local name = vim.api.nvim_buf_get_name(0)
-      local run = Terminal:new({
-        cmd = "python3 " .. name,
-        hidden = true,
-        direction = float,
-        close_on_exit = false,
-      })
-      run:toggle()
-    end
-
     vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua Lazygit_toggle()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>jj", "<cmd>TermExec cmd='lua %'<CR>", { noremap = true, silent = true })
   end,
 }
