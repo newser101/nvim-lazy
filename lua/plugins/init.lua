d870059216d6522b248f52ccc78ee850ea093718
newser101 <nepumuk101@gmail.com>
2023-10-28 16:01:04 +0200

    add twilight and zenmode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,4 +14,29 @@ return {
       { "<leader>tt", "<cmd>SymbolsOutline<CR>", desc = "Toggle symbols " },
     },
   },
+  -- Lua
+  {
+    "folke/twilight.nvim",
+    -- Twilight: toggle twilight
+    -- TwilightEnable: enable twilight
+    -- TwilightDisable: disable twilightjk
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  -- Lua
+  {
+    "folke/zen-mode.nvim",
+    -- Toggle Zen Mode with :ZenMode
+    opts = {
+      options={
+        number=true,
+      }
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
