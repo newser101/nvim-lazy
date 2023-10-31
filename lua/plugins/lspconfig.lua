c07e8c7e8d23b44f877f301fa829902e184bd495
newser101 <nepumuk101@gmail.com>
2023-11-01 00:10:25 +0100

    going on

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,15 +6,15 @@ return {
   dependencies = {
     -- { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
     -- from Lazyvim
-    { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies={"nvim-lspconfig"} },
+    { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
     { "folke/neodev.nvim",  opts = {} },
     -- "hrsh7th/cmp-nvim-lsp",
   },
   -- enabled = true,
 
   config = function()
-    -- global keymaps
     -- TODO: maybe rethink of better/handy/logic keymaps lhs definition
+    -- global keymaps
     vim.keymap.set("n", "gl", vim.diagnostic.open_float)
     vim.keymap.set("n", "<leader>lk", vim.diagnostic.goto_prev)
     vim.keymap.set("n", "<leader>lj", vim.diagnostic.goto_next)
@@ -27,6 +27,9 @@ return {
     local lspconfig = require("lspconfig")
     -- list of language servers
     local servers = require("lsp.lspserver")
+
+
+
     -- lspconfig.pyright.setup {}  -- connest to server
     require("lspconfig").pyright.setup({
       on_attach = function()
@@ -35,6 +38,30 @@ return {
       end,
     }) -- connect to server
     require("lspconfig").lua_ls.setup({
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = "LuaJIT",
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { "vim" },
+          },
+          workspace = { -- NOTE: need for 'luv' bug
+            checkThirdParty = false,
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          completion = {
+            callSnippet = "Replace",
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+
       on_attach = function()
         print("lua attached")
         vim.keymap.set("n", "K", vim.lsp.buf.hover(), { buffer = 0 })
