169f3b909e119a5d1fd723c8ec1b469bed93c64e
newser101 <nepumuk101@gmail.com>
2023-11-02 14:26:37 +0100

    edit lsp

--- lua/lsp/lua_ls.lua
+++ lua/lsp/lua_ls.lua
@@ -17,7 +17,17 @@ return {
       workspace = { -- NOTE: need for 'luv' bug
         checkThirdParty = false,
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
+        library = {
+          -- taken from josean-dev
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+
+          -- orig
+          -- vim.env.VIMRUNTIME
+
+          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+          -- vim.api.nvim_get_runtime_file("", true),
+        },
       },
       completion = {
         callSnippet = "Replace",
