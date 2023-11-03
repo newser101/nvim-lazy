4524e4ce6fee39d8aef205303e27d88c84778e78
newser101 <nepumuk101@gmail.com>
2023-11-03 21:05:26 +0100

    new smaller lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,13 +1,8 @@
 -- https://blog.codeminer42.com/configuring-language-server-protocol-in-neovim/
 return {
   "neovim/nvim-lspconfig",
-  -- lazy = true,
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    -- { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-    -- from Lazyvim
-    { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
-    { "folke/neodev.nvim",  opts = {} },
     "hrsh7th/cmp-nvim-lsp",
   },
   -- enabled = true,
@@ -16,71 +11,49 @@ return {
   -- https://github.com/LunarVim/Launch.nvim/blob/master/lua/user/lspconfig.lua
 
   config = function()
-    ---- global kemaps
-    -- h: vim.diagnostic
-    -- TODO: maybe rethink of better/handy/logic keymaps lhs definition
-    vim.keymap.set("n", "gl", vim.diagnostic.open_float)
-    vim.keymap.set("n", "<leader>lk", vim.diagnostic.goto_prev)
-    vim.keymap.set("n", "<leader>lj", vim.diagnostic.goto_next)
-    vim.keymap.set("n", "<leader>ll", vim.diagnostic.setloclist)
-
-    --  capabilities supported by nvim-cmp
-    --  show  handlers:
-    --  :lua vim.print(vim.tbl_keys(vim.lsp.handlers))
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
-    capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-
-    -- local Keymaps to parse to lsp setup
-    local function lsp_keymaps(bufnr) -- to test
-      -- local opts = { noremap = true, silent = true, buffer = bufnr, desc = "lsp" }
-      local opts = { noremap = true, silent = true }
-      -- set buffer local mapping
-      local keymap = vim.api.nvim_buf_set_keymap
-      -- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- orig
-      keymap(bufnr,"n", "gD", vim.lsp.buf.declaration, opts)
-      keymap(bufnr,"n", "gd", vim.lsp.buf.definition, opts)
-      -- keymap(bufnr,"n", "K", vim.lsp.buf.hover, opts)
-      keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-      keymap(bufnr,"n", "gi", vim.lsp.buf.implementation, opts)
-      keymap(bufnr,"n", "<leader>ls", vim.lsp.buf.signature_help, opts)
-      -- TODO: check if workspace is needed
-      --      vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-      --      vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-      --      vim.keymap.set('n', '<space>wl', function()
-      --        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      --      end, opts)
-      vim.keymap.set("n", "<leader>ld", vim.lsp.buf.type_definition, opts)
-      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-      vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { noremap = true, buffer = bufnr }) -- no silent
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-      vim.keymap.set("n", "<leader>lf", function()
-        vim.lsp.buf.format({ async = true })
-      end, opts)
-    end
-
     local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local servers = require("lsp.lspserver")
-    local on_attach = function(client, bufnr)
-      -- vim.print(client)
-      lsp_keymaps(bufnr)
-      -- NOTE:check illuminate
-      require("illuminate").on_attach(client)
+
+    local function lsp_keymaps(bufnr)
+      local opts = { noremap = true, silent = true }
+      local keymap = vim.api.nvim_buf_set_keymap
+      -- :TODO check better keymap
+      keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+      keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+      keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+      keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+      keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+      keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+      keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+      -- keymap(bufnr, "n", "<space>lf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
     end
+
+    local function on_attach(client, bufnr)
+      lsp_keymaps(bufnr)
+    end
+
+    -- Change diagnostic symbols in the sign column (gutter)
+    local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
     for _, server in pairs(servers) do
-      Opts = {
+      local opts = {
         on_attach = on_attach,
         capabilities = capabilities,
       }
-      server = vim.split(server, "@")[1]
 
       local require_ok, conf_opts = pcall(require, "lsp." .. server)
       if require_ok then
-        Opts = vim.tbl_deep_extend("force", conf_opts, Opts)
+        opts = vim.tbl_deep_extend("force", conf_opts, opts)
       end
-      lspconfig[server].setup(Opts)
+
+      lspconfig[server].setup(opts)
     end
   end,
 }
