d559780c6c730782e86080e92c89825242aae711
newser101 <nepumuk101@gmail.com>
2023-11-17 21:45:34 +0100

    tt

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -49,7 +49,7 @@ return {
       },
       snippet = {
         expand = function(args)
-          luasnip.lsp_expand(args.body)
+          require("luasnip").lsp_expand(args.body)
         end,
       },
       --  td
