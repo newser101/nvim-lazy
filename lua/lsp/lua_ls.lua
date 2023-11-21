8369c7c3ccc17852c97f756eb72346dd33d15e07
newser101 <nepumuk101@gmail.com>
2023-11-21 17:10:42 +0100

    ediot

--- lua/lsp/lua_ls.lua
+++ lua/lsp/lua_ls.lua
@@ -4,31 +4,46 @@ return {
   -- should be
   -- client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
 
+  -- ORIG Start
+  -- settings = {
+  --   Lua = {
+  --     runtime = {
+  --       -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+  --       version = 'LuaJIT',
+  --     },
+  --     diagnostics = {
+  --       -- Get the language server to recognize the `vim` global
+  --       globals = { 'vim' },
+  --     },
+  --     workspace = { -- NOTE: need for 'luv' bug
+  --       checkThirdParty = false,
+  --       -- Make the server aware of Neovim runtime files
+  --       library = {
+  --         -- taken from josean-dev
+  --         [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+  --         [vim.fn.stdpath("config") .. "/lua"] = true,
+  --
+  --         -- orig
+  --         -- vim.env.VIMRUNTIME
+  --
+  --         -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+  --         -- vim.api.nvim_get_runtime_file("", true),
+  --       },
+  --     },
+  --     completion = {
+  --       callSnippet = "Replace",
+  --     },
+  --     telemetry = {
+  --       enable = false
+  --     },
+  --   },
+  -- },
+  -- ORIG End
+
+
+  -- neoconf start
   settings = {
     Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
-      },
-      workspace = { -- NOTE: need for 'luv' bug
-        checkThirdParty = false,
-        -- Make the server aware of Neovim runtime files
-        library = {
-          -- taken from josean-dev
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.stdpath("config") .. "/lua"] = true,
-
-          -- orig
-          -- vim.env.VIMRUNTIME
-
-          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-          -- vim.api.nvim_get_runtime_file("", true),
-        },
-      },
       completion = {
         -- comment callsnippet an add snippetSupport
         -- callSnippet = "Replace",
